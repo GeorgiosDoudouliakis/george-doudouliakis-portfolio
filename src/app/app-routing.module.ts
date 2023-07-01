14c5553d308c7fbbd067d7bc41c7441c84dbcafe
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-07-01 21:02:17 +0300

    refactor: Layout and styling of home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+/* Place your angular imports here */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    loadComponent: () => import("./pages/home/home.component").then(({ HomeComponent }) => HomeComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+/* Place your angular imports here */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {}
