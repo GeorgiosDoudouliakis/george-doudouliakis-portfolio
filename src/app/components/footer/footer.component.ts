2b1ab9c2b2aca4e7899e629853f6b0bd8a77e27a
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-07-02 21:43:49 +0300

    refactor: Add go top arrow on footer, change border style to dashed on footer, and on social media buttons on home page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,8 +5,20 @@ import { Component } from '@angular/core';
   selector: 'portfolio-footer',
   standalone: true,
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  providers: [
+    {
+      provide: Window,
+      useValue: window
+    }
+  ]
 })
 export class FooterComponent {
   public currentYear = new Date().getFullYear();
+
+  constructor(private _window: Window) {}
+
+  public goTop(): void {
+    this._window.scrollTo(0, 0);
+  }
 }
