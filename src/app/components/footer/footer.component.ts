fed110f4972c6d3a9552852bb7a0eb5dcac7c802
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-07-02 21:48:20 +0300

    refactor: Import router module on footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 /* Place your angular imports here */
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'portfolio-footer',
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  imports: [RouterModule],
   providers: [
     {
       provide: Window,
