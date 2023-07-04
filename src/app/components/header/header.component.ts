3d3785b0557a35dd56edf4bcdf6f6c910a49a524
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-07-04 21:40:46 +0300

    refactor: Handle body overflow

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 /* Place your angular imports here */
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -8,16 +8,32 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  providers: [
+    {
+      provide: Window,
+      useValue: window
+    }
+  ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewChecked {
   public isMobileMenuOpen: boolean = false;
 
+  constructor(private _window: Window) {}
+
+  public ngAfterViewChecked(): void {
+    this._handleBodyOverflow();
+  }
+  
   public toggleMobileMenu(): void {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
-
+  
   public closeMobileMenu(): void {
     this.isMobileMenuOpen = false;
+  }
+  
+  private _handleBodyOverflow() {
+    this._window.document.body.style.overflow = this.isMobileMenuOpen ? "hidden" : "auto";
   }
 }
