800f91c94a8846ecc9065de532203b8cb3c72340
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-07-10 23:29:23 +0300

    refactor: Add a theme changer component without layout

---
+++ src/app/components/theme-changer/theme-changer.component.ts
@@ -0,0 +1,14 @@
+/* Place your angular imports here */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-theme-changer',
+  standalone: true,
+  templateUrl: './theme-changer.component.html',
+  styleUrls: ['./theme-changer.component.scss']
+})
+export class ThemeChangerComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit(): void {}
+}
