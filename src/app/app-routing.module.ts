1f248fa4d3cde4b95e9331d7925541f2f535b5da
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-07-15 15:23:23 +0300

    refactor: Add titles on routes and change index title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,17 @@ const routes: Routes = [
   {
     path: "",
     pathMatch: "full",
+    title: "George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/home/home.component").then(({ HomeComponent }) => HomeComponent)
   },
   {
     path: "about",
+    title: "About | George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/about/layout/about.component").then(({ AboutComponent }) => AboutComponent)
   },
   {
     path: "experience",
+    title: "Experience | George Doudouliakis - Front End Web Developer",
     loadComponent: () => import("./pages/experience/layout/experience.component").then(({ ExperienceComponent }) => ExperienceComponent)
   }
 ];
