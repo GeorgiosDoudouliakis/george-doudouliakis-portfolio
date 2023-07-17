91f5d99ef6674821762824ed1ebf0eecf7445073
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-07-17 19:23:27 +0300

    refactor: Update image paths

--- src/app/pages/about/mock/skills.mock.ts
+++ src/app/pages/about/mock/skills.mock.ts
@@ -5,42 +5,42 @@ export const SKILLS: Skill[] = [
   {
     name: 'HTML',
     image: {
-      path: '/assets/images/skills/html_logo.png',
+      path: 'assets/images/skills/html_logo.png',
       alt: 'HTML logo'
     }
   },
   {
     name: 'CSS',
     image: {
-      path: '/assets/images/skills/css_logo.png',
+      path: 'assets/images/skills/css_logo.png',
       alt: 'CSS logo'
     }
   },
   {
     name: 'JavaScript',
     image: {
-      path: '/assets/images/skills/js_logo.png',
+      path: 'assets/images/skills/js_logo.png',
       alt: 'JavaScript logo'
     }
   },
   {
     name: 'TypeScript',
     image: {
-      path: '/assets/images/skills/typescript_logo.png',
+      path: 'assets/images/skills/typescript_logo.png',
       alt: 'TypeScript logo'
     }
   },
   {
     name: 'Angular 2+',
     image: {
-      path: '/assets/images/skills/angular_logo.png',
+      path: 'assets/images/skills/angular_logo.png',
       alt: 'Angular 2+ logo'
     }
   },
   {
     name: 'SASS',
     image: {
-      path: '/assets/images/skills/sass_logo.png',
+      path: 'assets/images/skills/sass_logo.png',
       alt: 'SASS logo'
     }
   }
